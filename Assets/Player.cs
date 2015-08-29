f8251893c8a1d4822b019bbf372f3659717643f6
guoqiao <guoqiao@gmail.com>
2015-08-29 22:08:38 +1200

    mv fatHigh and fatLow value to unity

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,8 +7,8 @@ public class Player : Character {
 
     public int fatBurned = 0;
 
-    private int fatHigh = 5000;
-    private int fatLow = 2000;
+    public int fatHigh;
+    public int fatLow;
 
     private int _fat = 100;
     public int fat
