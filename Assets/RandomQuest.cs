92d4ad7197ffe60dd566d5a3f8428cee96e4494d
guoqiao <guoqiao@gmail.com>
2015-08-29 19:42:18 +1200

    add random quest

---
+++ Assets/RandomQuest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomQuest : Quest {
+    private bool firstEnter = true;
+    public override void begin() {
+        gameObject.SetActive(true);
+        if (firstEnter) {
+            firstEnter = false;
+        }
+        else {
+            //willRemoveSelf = true;
+            exit(posX, posY);
+        }
+        posX = (int)Random.Range(-5, 5);
+        posY = (int)Random.Range(-5, 5);
+
+        enter(posX, posY);
+        Debug.Log("random begin: " + posX + " " + posY);
+    }
+}
